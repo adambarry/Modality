d34ad04e677222a42fd6fe5730c383f6a107007d
Adam Barry <admbarry@gmail.com>
2014-04-03 21:02:18 +0200

    housekeeping

--- js/modal.js
+++ js/modal.js
@@ -115,9 +115,6 @@ var Modal = function (options, evt) {
         margin: 5, //Minimum distance in px between the viewport border and the modal-window. Modal-window will shrink when too little space
         disablePageScroll: true, //When set to true, body.style.overflow will be set to hidden, while modal-windows are active
         verticalCenter: false, //When set to true, the popupWindow will be centered vertically, instead of being placed towards the top of the screen
-        //loaderElement: false,
-        //showLoader: false,
-        //loaderMessage: false,
         fadeTime: 250, //Milliseconds for CSS3-based fade-in/out for the modal element
         fadeHiddenClass: "modal--hidden",
         closeLink: "<span>Close <span class=\"hotkey\">(esc)</span></span>" //Contents of permanent close button for the modal-window
